0c57fa7a476cc00ed978f2dbf85f4a6a6b44e6f8
Mark Kopysov <26510108+juggeroid@users.noreply.github.com>
2020-06-20 22:08:15 +0000

    Update naval-battle.rs

--- naval-battle.rs
+++ naval-battle.rs
@@ -1,10 +1,10 @@
 
 
 use itertools::Itertools;
-use rand::distributions::{Distribution, Standard, Uniform};
-use rand::rngs::{StdRng, ThreadRng};
-use rand::thread_rng;
-use rand::{Rng, SeedableRng};
+use rand::rngs::ThreadRng;
+use rand::Rng;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 const FIELD_SIZE: isize = 10;
 #[rustfmt::skip]
@@ -18,22 +18,22 @@ enum CellType {
 }
 
 struct Field {
-    field: Vec<Vec<CellType>>,
+    field: [CellType; 100],
 }
 
 impl std::fmt::Display for Field {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Ok(for row in &self.field {
-            for element in row {
-                #[rustfmt::skip]
-                let char_repr = match element {
-                    CellType::EMPTY       => ' ', 
-                    CellType::UNAVAILABLE => '.',
-                    CellType::OCCUPIED    => 'X',
-                };
-                write!(f, "{}", char_repr)?;
+        Ok(for (index, element) in self.field.iter().enumerate() {
+            #[rustfmt::skip]
+            let char_repr = match element {
+                CellType::EMPTY       => ' ',
+                CellType::UNAVAILABLE => '.',
+                CellType::OCCUPIED    => 'X',
+            };
+            if index % FIELD_SIZE as usize == 0 {
+                writeln!(f)?;
             }
-            writeln!(f)?;
+            write!(f, "{}", char_repr)?;
         })
     }
 }
@@ -52,14 +52,12 @@ fn is_valid_formation(
         let x = x + (dx * ship_size as isize);
         let y = y + (dy * ship_size as isize);
         for direction in DIRECTIONS.iter() {
-            let out_of_bounds: bool =
-                !bounds.contains(&(x + direction.0)) || !bounds.contains(&(y + direction.1));
             // Indices cannot be negative.
-            if out_of_bounds {
+            if !bounds.contains(&(x + direction.0)) || !bounds.contains(&(y + direction.1)) {
                 continue;
             }
             let bounding_box_cell =
-                field.field[(x + direction.0) as usize][(y + direction.1) as usize];
+                field.field[((x + direction.0) + ((y + direction.1) * FIELD_SIZE)) as usize];
             if bounding_box_cell == CellType::OCCUPIED {
                 return false;
             }
@@ -67,18 +65,17 @@ fn is_valid_formation(
     }
     /* II. Check whether the cells that are being used to place the ship are occupied. */
     for _ in 0..ship_size {
-        let out_of_bounds = !bounds.contains(&x) || !bounds.contains(&y);
-        if out_of_bounds {
+        if !bounds.contains(&x) || !bounds.contains(&y) {
             return false;
         }
-        let current_cell = field.field[x as usize][y as usize];
+        let current_cell = field.field[(y * FIELD_SIZE + x) as usize];
         if let CellType::OCCUPIED | CellType::UNAVAILABLE = current_cell {
             return false;
         }
         x += dx;
         y += dy;
     }
-    return true;
+    true
 }
 
 fn get_available_cells(
@@ -94,20 +91,12 @@ fn get_available_cells(
 }
 
 fn emplace_ships(field: &mut Field, ship_size: usize, rng: &mut ThreadRng) {
-    let coin_toss: bool = rng.gen_bool(0.5);
-    // Decide the ship's alignment (horizontal / vertical, with equal probability).
-    let (mut dx, mut dy): (isize, isize) = (0, 0);
-    if coin_toss {
-        dx = 1;
-    } else {
-        dy = 1;
-    }
+    let (dx, dy) = if rng.gen() { (1, 0) } else { (0, 1) };
     let cell_coordinates = get_available_cells(&field, dx, dy, ship_size);
     let chosen_cell = cell_coordinates[rng.gen_range(0, cell_coordinates.len())];
-    let mut x = chosen_cell.0;
-    let mut y = chosen_cell.1;
+    let (mut x, mut y) = (chosen_cell.0, chosen_cell.1);
     for _ in 0..ship_size {
-        field.field[x as usize][y as usize] = CellType::OCCUPIED;
+        field.field[(x + y * FIELD_SIZE) as usize] = CellType::OCCUPIED;
         x += dx;
         y += dy;
     }
@@ -116,18 +105,17 @@ fn emplace_ships(field: &mut Field, ship_size: usize, rng: &mut ThreadRng) {
 impl Field {
     fn generate() -> Self {
         /* Generating the field. */
-        let mut f =
-            Field { field: vec![vec![CellType::EMPTY; FIELD_SIZE as usize]; FIELD_SIZE as usize] };
+        let mut f = Field { field: [CellType::EMPTY; 100] };
         let mut rng: ThreadRng = rand::thread_rng();
         for ship_size in (1..=4).rev().flat_map(|c| std::iter::repeat(c).take(5 - c)) {
             emplace_ships(&mut f, ship_size, &mut rng);
         }
-        println!("{}", f);
-        todo!()
+        f
     }
 }
-
 fn main() {
+    let now = Instant::now();
     let field = Field::generate();
-    println!("{}", field)
+    let new = Instant::now();
+    println!("{:?}", new.duration_since(now));
 }

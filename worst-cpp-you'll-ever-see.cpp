d7dd7a64c098a7f6f6d3b3cde4062c28ba6a4f9c
M K <26510108+juggeroid@users.noreply.github.com>
2021-01-28 20:30:59 +0000

    please kill me

--- worst-cpp-you'll-ever-see.cpp
+++ worst-cpp-you'll-ever-see.cpp
@@ -1,14 +1,31 @@
-
-
-#include <iostream>
-#include <vector>
 #include <array>
-#include <random>
 #include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include <boost/container/static_vector.hpp>
+#include <boost/random.hpp>
+
+template <typename Type = std::uint64_t> class randomizer_with_sentinel_shift {
+  public:
+    template <typename Generator> bool operator()(Generator &rng) {
+        if (1 == m_rand)
+            m_rand = std::uniform_int_distribution<Type> {} (rng) | s_mask_left1;
+        const bool ret = m_rand & 1;
+        m_rand >>= 1;
+        return ret;
+    }
+  private:
+    static constexpr Type s_mask_left1 = Type(1) << (sizeof(Type) * 8 - 1);
+    Type m_rand = 1;
+};
 
 namespace {
-    static constexpr auto FIELD_SIZE = 10;
-    enum CellType {
+  
+    constexpr auto FIELD_SIZE = 10;
+    enum class CellType: std::uint8_t {
       EMPTY,
       UNAVAILABLE,
       OCCUPIED
@@ -16,25 +33,32 @@ namespace {
     using Coordinates = std::pair<std::int32_t, std::int32_t>;
     using Directions = std::array<Coordinates, 9>;
 
-    static std::minstd_rand generator{ std::random_device{}() };
-    static std::uniform_int_distribution distribution(0, FIELD_SIZE);
-    static std::uniform_int_distribution bool_distribution(0, 1);
+    constexpr auto DIRECTIONS = Directions {{{  0,  0 },
+                                             {  0,  1 },
+                                             {  0, -1 },
+                                             { -1,  0 },
+                                             {  1,  0 },
+                                             { -1,  1 },
+                                             {  1, -1 },
+                                             { -1, -1 },
+                                             {  1,  1 }}};
+
+    boost::taus88 generator {std::random_device{}()};
+    randomizer_with_sentinel_shift<std::uint64_t> bool_generator;
 }
 
-struct Field
-{
-  std::array<CellType, FIELD_SIZE* FIELD_SIZE> field = { EMPTY };
-  friend std::ostream& operator<<(std::ostream& o, const Field& field)
-  {
+struct Field {
+  std::array<CellType, FIELD_SIZE * FIELD_SIZE> field = {CellType::EMPTY };
+  friend std::ostream& operator<<(std::ostream& o, const Field& field) {
     for (size_t index = 0; index < FIELD_SIZE * FIELD_SIZE;) {
       switch (field.field[index]) {
-        case EMPTY:
+        case CellType::EMPTY:
           o << ".";
           break;
-        case UNAVAILABLE:
+        case CellType::UNAVAILABLE:
           o << " ";
           break;
-        case OCCUPIED:
+        case CellType::OCCUPIED:
           o << "X";
           break;
       }
@@ -46,43 +70,40 @@ struct Field
 };
 
 bool
-is_valid_formation(const Field& field, std::size_t x, std::size_t y,
-                   std::int32_t dx, std::int32_t dy,
-                   std::size_t ship_size) noexcept
+is_valid_formation(const Field&       field, 
+                   std::size_t        x, 
+                   std::size_t        y,
+                   const std::int32_t dx, 
+                   const std::int32_t dy,
+                   const std::size_t  ship_size) noexcept
 {
 
-  static constexpr auto DIRECTIONS = Directions {{{  0,  0 },
-                                                  {  0,  1 },
-                                                  {  0, -1 },
-                                                  { -1,  0 },
-                                                  {  1,  0 },
-                                                  { -1,  1 },
-                                                  {  1, -1 },
-                                                  { -1, -1 },
-                                                  {  1,  1 }}};
-
-  const auto within_bounds = [&]
-  (const auto x, const auto y, const std::pair<std::int32_t, std::int32_t>& direction = {0, 0})
-  {
-    const bool xbound = (x + direction.first  < FIELD_SIZE) && (x + direction.first  >= 0);
-    const bool ybound = (y + direction.second < FIELD_SIZE) && (y + direction.second >= 0);
-    return xbound && ybound;
+  const auto within_bounds = [&](const auto x, 
+                                 const auto y, 
+                                 const std::pair<std::int32_t, std::int32_t>& direction = std::pair {0, 0}) noexcept {
+    return ((x + direction.first  < FIELD_SIZE) && (x + direction.first  >= 0))
+           && 
+           ((y + direction.second < FIELD_SIZE) && (y + direction.second >= 0));
   };
 
   for (std::size_t iteration = 0; iteration < ship_size; ++iteration) {
     const std::int32_t nx = x + (dx * iteration);
     const std::int32_t ny = y + (dy * iteration);
     for (const auto& direction: DIRECTIONS) {
-      if (!within_bounds(nx, ny, direction)) continue;
+      [[unlikely]] if (!within_bounds(nx, ny, direction))
+      continue;
       const auto& bounding_box_cell = field.field[(nx + direction.first) + ((ny + direction.second) * FIELD_SIZE)];
-      if (OCCUPIED == bounding_box_cell) return false;
+      if (CellType::OCCUPIED == bounding_box_cell) 
+      return false;
     }
   }
-
+  
   for (std::size_t iteration = 0; iteration < ship_size; ++iteration) {
-    if (!within_bounds(x, y)) return false;
+    [[unlikely]] if (!within_bounds(x, y))
+    return false;
     const auto& current_cell = field.field[(y * FIELD_SIZE) + x];
-    if (OCCUPIED == current_cell || UNAVAILABLE == current_cell) return false;
+    if (CellType::OCCUPIED == current_cell || CellType::UNAVAILABLE == current_cell)
+    return false;
     x += dx;
     y += dy;
   }
@@ -90,39 +111,36 @@ is_valid_formation(const Field& field, std::size_t x, std::size_t y,
 }
 
 void
-get_available_cells(const Field& field, std::int32_t dx, std::int32_t dy,
-                    std::size_t ship_size,
-                    std::vector<Coordinates>& buffer) noexcept
+get_available_cells(const Field&       field, 
+                    const std::int32_t dx, 
+                    const std::int32_t dy,
+                    const std::size_t  ship_size,
+                    boost::container::static_vector<Coordinates, FIELD_SIZE * FIELD_SIZE>& buffer) noexcept
 {
   buffer.clear();
   for (std::size_t x = 0; x < FIELD_SIZE; ++x) {
     for (std::size_t y = 0; y < FIELD_SIZE; ++y) {
       if (is_valid_formation(field, x, y, dx, dy, ship_size))
-        buffer.push_back({ x, y });
+        buffer.emplace_back(x, y);
     }
   }
 }
 
-auto emplace_ships(Field& field, std::size_t ship_size,
-                   std::vector<Coordinates>& buffer) noexcept
+auto emplace_ships(Field& field, 
+                   const std::size_t ship_size,
+                   boost::container::static_vector<Coordinates, FIELD_SIZE * FIELD_SIZE>& buffer) noexcept
 {
   const auto get_alignment = [&] {
-    return bool_distribution(generator) ? Coordinates {1, 0}
-                                        : Coordinates {0, 1};
-  };
-
-  const auto pick_random = [&](auto begin, auto end) noexcept
-  {
-    std::uniform_int_distribution<> distribution(0, std::distance(begin, end) - 1);
-    std::advance(begin, distribution(generator));
-    return begin;
+    return bool_generator(generator) 
+           ? Coordinates {1, 0}
+           : Coordinates {0, 1};
   };
 
   auto [dx, dy] = get_alignment();
   get_available_cells(field, dx, dy, ship_size, buffer);
-  auto [x, y] = *pick_random(std::begin(buffer), std::end(buffer));
+  auto [x, y] = buffer[generator() % buffer.size()];
   for (std::size_t iteration = 0; iteration < ship_size; ++iteration) {
-    field.field[x + (y * FIELD_SIZE)] = OCCUPIED;
+    field.field[x + (y * FIELD_SIZE)] = CellType::OCCUPIED;
     x += dx;
     y += dy;
   }
@@ -131,15 +149,10 @@ auto emplace_ships(Field& field, std::size_t ship_size,
 int
 main()
 {
-  auto start = std::chrono::high_resolution_clock::now();
-  Field field;
-  std::vector<Coordinates> buffer;
+  boost::container::static_vector<Coordinates, FIELD_SIZE * FIELD_SIZE> buffer;
   buffer.resize(FIELD_SIZE * FIELD_SIZE);
-  for (const auto& ship_size: { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 }) {
+  Field field;
+  for (auto&& ship_size: { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 }) {
     emplace_ships(field, ship_size, buffer);
   }
-  auto elapsed = std::chrono::high_resolution_clock::now() - start;
-  long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-  std::cout << microseconds << " ms.\n";
-  std::cout << field;
 }
